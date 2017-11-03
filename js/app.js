fb3dcf5e52c5eed841da59f9767c0e474813aeb4
Samuel Nuttall <sndrummer@gmail.com>
2017-11-02 20:41:53 -0600

    need to add NODE

--- js/app.js
+++ js/app.js
@@ -1,23 +1,56 @@
 
     var app = angular.module('app',[]);
+    var race= "";
     
     app.controller('formCtrl', function($scope) {
         $scope.characterName = "";
+        //XP
+  
     });
+
+    app.controller('xpController', ['$scope', function($scope) {
+    
+      $scope.StartXP = 1;
+      $scope.EarnedXP = 2;
+      $scope.TotalXP = 3;
+      $scope.UsedXP = 4;
+      $scope.UnusedXP = 5;
+  }])
+  app.directive('xpDirective', function() {
+
+
+    return {
+      restrict: 'E',
+      scope: { model: '=' },
+      template: '<select class="form-control" id="species" ng-model="model" ng-options="option.name as option.value group by option.type for option in options"></select>',
+      controller: function($scope) {
+        
+      }    
+    };
+
+
+  });
+
+
+
+
+
+
+
    
     app.controller('appController', ['$scope', function($scope) {
-          $scope.species = {};
-          $scope.species.selection = '';
       }])
-      app.directive('theRatings', function() {
-        
+      app.directive('speciesList', function() {
+        //var mySpec = $scope.species;
+        //console.log(mySpec);
         return {
           restrict: 'E',
           scope: { model: '=' },
-          template: '<select class="form-control" id="species" ng-model="model" ng-options="option.name as option.value group by option.type for option in options"></select>',
+          template: '<select class="form-control" id="species" ng-change="changeRace()" ng-model="species_selection" ng-options="option.name as option.value group by option.type for option in options"></select>',
           controller: function($scope) {
             $scope.options = [
-                { name: 'Aqualish', value: 'Aqualish' , type:""}, 
+              { name: '', value: '' , type:""},  
+              { name: 'Aqualish', value: 'Aqualish' , type:""}, 
                 { name: 'Bothan', value: 'Bothan' , type:""}, 
                 { name: 'Chevin', value: 'Chevin' , type:""}, 
                 { name: 'Chiss', value: 'Chiss' , type:""}, 
@@ -39,6 +72,19 @@
                 { name: 'Weequay', value: 'Weequay' , type:""}, 
                 { name: 'Wookiee', value: 'Wookiee', type:"" }
             ];
+
+            $scope.changeRace = function() {
+              console.log("Function called");
+              var mySpec = $scope.species_selection;
+              console.log(mySpec);
+
+
+              function updateValues(mySpec){
+                if (mySpec) {
+                  makeQuery(mySpec);
+                }
+              }
+            };
           }    
         };
       });
